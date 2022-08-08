f558fe346cac30de249d2fbe77a7a341937e40b4
Jashon Osala <jashonosala@gmail.com>
2022-08-09 02:34:57 +0300

    Handle betty

--- nums.c
+++ nums.c
@@ -19,17 +19,11 @@ unsigned int convert_di(va_list args, buffer_t *output,
 	unsigned int ret = 0, count = 0;
 	char pad, space = ' ', neg = '-', plus = '+';
 
-	if (len == LONG)
-		d = va_arg(args, long int);
-	else
-		d = va_arg(args, int);
+	d = len == LONG ? va_arg(args, long int) : va_arg(args, int);
 	if (len == SHORT)
 		d = (short)d;
-
-	/* Handle space flag */
 	if (SPACE_FLAG == 1 && d >= 0)
 		ret += _memcpy(output, &space, 1);
-
 	if (prec <= 0 && NEG_FLAG == 0) /* Handle width  */
 	{
 		if (d == LONG_MIN)
@@ -43,31 +37,22 @@ unsigned int convert_di(va_list args, buffer_t *output,
 		count += (d < 0) ? 1 : 0;
 		count += (PLUS_FLAG == 1 && d >= 0) ? 1 : 0;
 		count += (SPACE_FLAG == 1 && d >= 0) ? 1 : 0;
-
-		/* Handle plus flag when zero flag is active */
 		if (ZERO_FLAG == 1 && PLUS_FLAG == 1 && d >= 0)
 			ret += _memcpy(output, &plus, 1);
-		/*Print negative sign when zero flag is active */
 		if (ZERO_FLAG == 1 && d < 0)
 			ret += _memcpy(output, &neg, 1);
-
 		pad = (ZERO_FLAG == 1) ? '0' : ' ';
 		for (wid -= count; wid > 0; wid--)
 			ret += _memcpy(output, &pad, 1);
 	}
-	/* Print negative sign when zero flag is not active */
 	if (ZERO_FLAG == 0 && d < 0)
 		ret += _memcpy(output, &neg, 1);
-	/* Handle plus flag when zero flag is not active */
 	if (ZERO_FLAG == 0 && (PLUS_FLAG == 1 && d >= 0))
 		ret += _memcpy(output, &plus, 1);
-
 	if (!(d == 0 && prec == 0))
 		ret += convert_sbase(output, d, "0123456789",
 				flags, 0, prec);
-
 	ret += print_neg_width(output, ret, flags, wid);
-
 	return (ret);
 }
 
