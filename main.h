0d25aaac422a512ca8dbcd82c517a58c11cc04ab
Verah Mokaya <verahmoy@gmail.com>
2022-08-08 12:01:46 +0000

    Defined basics in main.h

--- main.h
+++ main.h
@@ -7,9 +7,36 @@
 #include <stdarg.h>
 
 /*
+ * Verah
  * _putchar
  * _puts
  * _printf
  */
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts - print a string using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
+
+void _puts(char *s);
+
+/**
+ * _printf - printf function
+ * @format: directives
+ *
+ * Return: Always 0 (Success)
+ */
+int _printf(const char *format, ...);
 
 #endif
