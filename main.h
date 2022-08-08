1aa6e3f8ae29a62d042b38cbaabf66066da2d640
Jashon Osala <jashonosala@gmail.com>
2022-08-08 15:05:57 +0300

    Main.h done

--- main.h
+++ main.h
@@ -39,4 +39,69 @@ void _puts(char *s);
  */
 int _printf(const char *format, ...);
 
+/**
+ * print_c - handle char as string
+ * @params: char
+ * Return: String verion of char
+ */
+char *print_c(va_list params);
+
+/**
+ * print_s - handle printing a string
+ * @params: params
+ * Return: string
+ */
+
+char *print_s(va_list params);
+
+/**
+ * _strlen - return string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s);
+
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Character
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * get_func - returns needed function
+ * @i: func key
+ * Return: Pointer to function
+ */
+char* (*get_func(char i))(va_list);
+
+/**
+ * create_buffer - creates buffer to hold string
+ * Return: pointer to buffer
+ */
+char *create_buffer(void);
+
+/**
+ * write_buffer - prints buffer, then frees it and frees va_list
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+void write_buffer(char *buffer, int len, va_list params);
+
+
+/**
+ * struct types - types
+ * @id: key
+ * @func: ptr to func
+ */
+
+typedef struct types
+{
+        char *id;
+        char* (*func)(va_list);
+} print;
 #endif
